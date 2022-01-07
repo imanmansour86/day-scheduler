2cdbb58d7a838039d62f268071aab62507ba1472
imanmansour86 <imanmansour86@gmail.com>
2022-01-06 18:45:48 -0800

    adding save functionality to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,35 @@
-console.log(moment());
 console.log(moment().format());
+var container = $(".container");
+
+//Display today's date on top of the page
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+
+function compareTime() {
+  var currentTime = moment().format("hh:mm"); //get current time
+  console.log(currentTime);
+
+  var first = "09:00";
+  var t = moment("9").format("hh:mm");
+  console.log(t);
+
+  console.log(moment("09:00").isBefore("12:00", "hour"));
+}
+
+container.on("click", ".save-button", function (event) {
+  console.dir(event.target.parentElement);
+
+  var enteredTxt = event.target.parentElement.children[1].value;
+
+  console.log(
+    "text entered by user is " +
+      event.target.parentElement.children[0].outerText
+  );
+
+  var currentTime = event.target.parentElement.children[0].outerText;
+
+  localStorage.setItem(currentTime, enteredTxt);
+});
+
+function getEvents() {
+  const allEvent = Object.entries(localStorage);
+}
